613885860e621d87488d351c6ca14aa165ae7e00
ZakGregory <zgregoryk@gmail.com>
2020-11-25 16:10:31 +0000

    some changes to dockerfiles+ urls

--- service1/app.py
+++ service1/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    animal_response = requests.get('http://35.246.77.235:5001/get/animal/').text
-    noise_response= requests.post('http://35.246.77.235:5001/post/animal/', animal_response).text
+    animal_response = requests.get('172.17.0.0:5001/get/animal/').text
+    noise_response= requests.post('172.17.0.0:5001/post/animal/', animal_response).text
     a= animal_response+noise_response
     return a
 
